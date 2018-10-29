c0e7a0bd6992808cfcd402177ddc47f823e9224a
Domingo Yang <DDDDDomingo2017@gmail.com>
2018-10-29 18:37:18 +0800

    数据库修改
    删除老版本，报名表新增字段pic

--- beita2018-10-29.sql
+++ beita2018-10-29.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50721
 File Encoding         : 65001
 
-Date: 2018-10-23 15:50:49
+Date: 2018-10-29 18:36:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,16 +21,16 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `admission_ticket_info`;
 CREATE TABLE `admission_ticket_info` (
   `ticket_info_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '考试信息id',
-  `userinfo_id` int(11) NOT NULL COMMENT '用户账号表关键字',
+  `userinfo_id` char(12) NOT NULL COMMENT '用户账号表关键字',
   `ticket_info_name` varchar(255) NOT NULL COMMENT '准考证id(需要一个生成算法)',
-  `ticket_info_identifier` int(18) NOT NULL,
+  `ticket_info_identifier` char(18) NOT NULL,
   `ticket_info_time` datetime NOT NULL COMMENT '考试开始时间',
   `ticket_info_duration` int(11) NOT NULL COMMENT '考试时间长度',
   `ticket_info_seatnum` int(3) NOT NULL COMMENT '考场座位号',
   `ticket_info_school` varchar(255) NOT NULL COMMENT '考试学校',
   PRIMARY KEY (`ticket_info_id`),
-  KEY `userinfo_id2` (`userinfo_id`),
   KEY `ticket_info_identifier` (`ticket_info_identifier`),
+  KEY `userinfo_id2` (`userinfo_id`),
   CONSTRAINT `userinfo_id2` FOREIGN KEY (`userinfo_id`) REFERENCES `user_info` (`userinfo_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -44,9 +44,9 @@ CREATE TABLE `admission_ticket_info` (
 DROP TABLE IF EXISTS `enter_personnel`;
 CREATE TABLE `enter_personnel` (
   `enter_p_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '考试录入表id',
-  `exam_type_id` int(11) NOT NULL COMMENT '考试类别表id',
-  `userinfo_id` int(11) NOT NULL COMMENT '被派发人员 id',
-  `enter_is_check` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否已经审核',
+  `exam_type_id` char(8) NOT NULL COMMENT '考试类别表id',
+  `userinfo_id` char(12) NOT NULL COMMENT '被派发人员 id',
+  `enter_is_check` tinyint(4) NOT NULL COMMENT '是否已经审核',
   PRIMARY KEY (`enter_p_id`),
   KEY `exam_type_id3` (`exam_type_id`),
   KEY `userinfo_id3` (`userinfo_id`),
@@ -67,7 +67,7 @@ CREATE TABLE `exam_news` (
   `etype_id` int(11) NOT NULL COMMENT '考试新闻类别表id',
   `news_content` text NOT NULL COMMENT '考试新闻内容（存储纯html）',
   `news_time` datetime NOT NULL COMMENT '考试新闻发布时间',
-  `news_isnew` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否为最新新闻',
+  `news_isnew` tinyint(4) NOT NULL COMMENT '是否为最新新闻',
   `news_visits` int(11) NOT NULL DEFAULT '0' COMMENT '考试新闻浏览次数，默认值为0',
   PRIMARY KEY (`news_id`),
   KEY `etype_id` (`etype_id`),
@@ -98,8 +98,8 @@ CREATE TABLE `exam_news_type` (
 DROP TABLE IF EXISTS `exam_score`;
 CREATE TABLE `exam_score` (
   `score_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '考试成绩表id',
-  `exam_type_id` int(11) NOT NULL COMMENT '考试类别表id',
-  `ticket_info_identifier` int(18) NOT NULL COMMENT '准考证id',
+  `exam_type_id` char(18) NOT NULL COMMENT '考试类别表id',
+  `ticket_info_identifier` char(18) NOT NULL COMMENT '准考证id',
   `score_num` varchar(11) NOT NULL COMMENT '成绩',
   PRIMARY KEY (`score_id`),
   KEY `exam_type_id4` (`exam_type_id`),
@@ -118,13 +118,13 @@ CREATE TABLE `exam_score` (
 DROP TABLE IF EXISTS `exam_session`;
 CREATE TABLE `exam_session` (
   `session_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '考试场次id',
-  `exam_type_id` int(11) NOT NULL COMMENT '考试类别表id',
+  `exam_type_id` char(8) NOT NULL COMMENT '考试类别表id',
   `session_place` varchar(255) NOT NULL COMMENT '考试场次地点',
   `session_time` datetime NOT NULL COMMENT '考试时间',
   `session_capacity` int(11) NOT NULL COMMENT '考场容量',
   PRIMARY KEY (`session_id`),
-  KEY `exam_type_id` (`exam_type_id`),
-  CONSTRAINT `exam_type_id` FOREIGN KEY (`exam_type_id`) REFERENCES `exam_type` (`exam_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `exam_type_id2` (`exam_type_id`),
+  CONSTRAINT `exam_type_id2` FOREIGN KEY (`exam_type_id`) REFERENCES `exam_type` (`exam_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -137,15 +137,16 @@ CREATE TABLE `exam_session` (
 DROP TABLE IF EXISTS `exam_signup_list`;
 CREATE TABLE `exam_signup_list` (
   `signup_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '考试报名表id',
-  `exam_type_id` int(11) NOT NULL COMMENT '考试类别表id',
-  `details_id` int(11) NOT NULL COMMENT '用户个人信息表id',
+  `exam_type_id` char(8) NOT NULL COMMENT '考试类别表id',
+  `signup_pic` varchar(255) NOT NULL COMMENT '考试费用提交凭证',
+  `details_id` char(12) NOT NULL COMMENT '用户个人信息表id',
   `signup_time` datetime NOT NULL COMMENT '考试报名时间',
-  `signup_isconfirm` tinyint(1) NOT NULL DEFAULT '0' COMMENT '审核是否通过',
+  `signup_isconfirm` tinyint(4) NOT NULL COMMENT '审核是否通过',
   PRIMARY KEY (`signup_id`),
-  KEY `exam_type_id2` (`exam_type_id`),
+  KEY `exam_type_id` (`exam_type_id`),
   KEY `details_id` (`details_id`),
   CONSTRAINT `details_id` FOREIGN KEY (`details_id`) REFERENCES `user_details` (`details_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `exam_type_id2` FOREIGN KEY (`exam_type_id`) REFERENCES `exam_type` (`exam_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `exam_type_id` FOREIGN KEY (`exam_type_id`) REFERENCES `exam_type` (`exam_type_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -157,19 +158,22 @@ CREATE TABLE `exam_signup_list` (
 -- ----------------------------
 DROP TABLE IF EXISTS `exam_type`;
 CREATE TABLE `exam_type` (
-  `exam_type_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '考试类别表id',
+  `exam_type_id` char(8) NOT NULL COMMENT '考试类别表id',
   `exam_name` varchar(255) NOT NULL,
   `exam_isclosed` tinyint(1) NOT NULL DEFAULT '0' COMMENT '考试是否已经结束',
   `exam_issignup` tinyint(1) NOT NULL DEFAULT '0' COMMENT '考试是否正在报名',
   `exam_isquery` tinyint(1) NOT NULL DEFAULT '0' COMMENT '考试是否可查询',
-  `exam_audited_num` int(11) NOT NULL COMMENT '考试已审核人数',
-  `exam_capacity` int(11) NOT NULL COMMENT '考试可容纳人数',
+  `exam_audited_num` int(11) NOT NULL DEFAULT '0' COMMENT '考试已审核人数',
+  `exam_capacity` int(11) NOT NULL DEFAULT '0' COMMENT '考试可容纳人数',
+  `exam_starttime` datetime NOT NULL,
+  `exam_endtime` datetime NOT NULL,
   PRIMARY KEY (`exam_type_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of exam_type
 -- ----------------------------
+INSERT INTO `exam_type` VALUES ('18102858', '计算机二级考试', '1', '0', '0', '0', '1000', '2018-10-28 00:00:00', '2018-11-28 00:00:00');
 
 -- ----------------------------
 -- Table structure for `operating`
@@ -265,7 +269,7 @@ CREATE TABLE `rel_ug_role` (
 DROP TABLE IF EXISTS `rel_ui_ug`;
 CREATE TABLE `rel_ui_ug` (
   `rel_ui_ug_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户与用户组关联id',
-  `userinfo_id` int(11) NOT NULL COMMENT '用户账号密码表关键字（外键）',
+  `userinfo_id` char(12) NOT NULL COMMENT '用户账号密码表关键字（外键）',
   `group_id` int(11) NOT NULL COMMENT '用户组表关键字',
   PRIMARY KEY (`rel_ui_ug_id`),
   KEY `group_id` (`group_id`),
@@ -302,11 +306,11 @@ CREATE TABLE `resource` (
 DROP TABLE IF EXISTS `system_notice`;
 CREATE TABLE `system_notice` (
   `notice_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '系统通知表id',
-  `notice_sender_id` int(11) NOT NULL COMMENT '发送者id',
-  `notice_receive_id` int(11) NOT NULL,
+  `notice_sender_id` char(12) NOT NULL COMMENT '发送者id',
+  `notice_receive_id` char(12) NOT NULL,
   `notice_content` varchar(255) NOT NULL COMMENT '消息内容',
   `notice_createtime` datetime NOT NULL COMMENT '消息时间',
-  `notice_isread` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否已读',
+  `notice_isread` tinyint(4) NOT NULL COMMENT '是否已读',
   PRIMARY KEY (`notice_id`),
   KEY `notice_sender_id` (`notice_sender_id`),
   KEY `notice_receive_id` (`notice_receive_id`),
@@ -323,8 +327,8 @@ CREATE TABLE `system_notice` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user_details`;
 CREATE TABLE `user_details` (
-  `details_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户个人信息表id',
-  `details_avatar` varchar(255) NOT NULL DEFAULT '127.0.0.1:8080/xxx/xxx' COMMENT '用户头像（有一个默认值）',
+  `details_id` char(12) NOT NULL COMMENT '用户个人信息表id',
+  `details_avatar` varchar(255) NOT NULL DEFAULT '110.10.1.1' COMMENT '用户头像（有一个默认值）',
   `details_phone` char(11) DEFAULT NULL COMMENT '用户手机号',
   `details_address` varchar(255) DEFAULT NULL COMMENT '用户个人地址',
   `details_realname` varchar(255) DEFAULT NULL COMMENT '用户真实姓名',
@@ -335,6 +339,7 @@ CREATE TABLE `user_details` (
 -- ----------------------------
 -- Records of user_details
 -- ----------------------------
+INSERT INTO `user_details` VALUES ('181028969487', '110.10.1.1', null, null, null, null);
 
 -- ----------------------------
 -- Table structure for `user_group`
@@ -355,16 +360,18 @@ CREATE TABLE `user_group` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info` (
-  `userinfo_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `userinfo_id` char(12) NOT NULL COMMENT '用户id',
   `userinfo_account` varchar(50) NOT NULL COMMENT '用户账号',
   `userinfo_password` varchar(16) NOT NULL COMMENT '用户密码',
-  `userinfo_email` varchar(255) NOT NULL DEFAULT 'xxxxx@xxx.xxx' COMMENT '用户邮箱（用来通知考试，必填）',
+  `userinfo_email` varchar(255) DEFAULT NULL COMMENT '用户邮箱（用来通知考试，必填）',
   PRIMARY KEY (`userinfo_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_info
 -- ----------------------------
+INSERT INTO `user_info` VALUES ('181028562814', 'admin', '123456', null);
+INSERT INTO `user_info` VALUES ('181028969487', 'account1', 'pwd123', '1783610921@qq.com');
 
 -- ----------------------------
 -- Table structure for `user_role`
