815645e48cd4ab98de11d6963032c18299e57aae
Domingo Yang <DDDDDomingo2017@gmail.com>
2018-11-06 16:06:03 +0800

    excel

--- beitasystem/beitasystem/src/main/java/studio/beita/hdxg/beitasystem/utils/ExcelUtils.java
+++ beitasystem/beitasystem/src/main/java/studio/beita/hdxg/beitasystem/utils/ExcelUtils.java
@@ -17,6 +17,7 @@ public class ExcelUtils {
     public static boolean isExcel2003(String filePath) {
         return filePath.matches("^.+\\.(?i)(xls)$");
     }
+
     public static boolean isExcel2007(String filePath) {
         return filePath.matches("^.+\\.(?i)(xlsx)$");
     }
